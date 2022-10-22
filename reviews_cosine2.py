e04b825f7b758e7eadedb67148106f6bfa52df31
Leonor Barreiros <leonorbarreiros@MBP-de-Leonor.home>
2022-10-22 17:18:15 +0100

    naive bayes

---
+++ reviews_cosine2.py
@@ -0,0 +1,137 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+# 2nd model: classify according to cosine similarity
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import sys
+from math import log
+from traceback import print_tb
+import pandas as pd
+import nltk
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from nltk.corpus import stopwords
+import json
+import numpy as np
+from numpy.linalg import norm
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# # # #
+# 1. Get train and test sets' files
+# # # #
+
+args = sys.argv[1:]
+
+i = 0
+for arg in args:
+    if arg == '-test':
+        test = args[i + 1]
+    if arg == '-train':
+        train = args[i + 1]
+    i += 1
+
+# # # #
+# 2. Get train & test features
+# # # #
+
+features = []
+
+with open(train) as f:
+    lines = f.readlines()
+
+for line in lines:
+    feature = line.split()[1:]
+    s = ""
+    for f in feature:
+        s += f + " "
+    s = s[:-1]
+    features.append(s)
+
+with open(test) as f:
+    lines = f.readlines()
+
+for line in lines:
+    features.append(line)
+
+# # # #
+# 3. Get train & test labels
+# # # #
+
+labels = []
+
+with open(train) as f:
+    lines = f.readlines()
+
+for line in lines:
+    label = line.split()[0]
+    labels.append(label)
+
+test_labels = []
+
+with open('test_results.txt') as f:
+    lines = f.readlines()
+
+for line in lines:
+    test_labels.append(line[:-1])
+
+# # # #
+# 4. Process reviews
+# # # #
+
+# 4.1 Lowercasing
+features = [feature.lower() for feature in features]
+
+# 4.2 Tokenization
+features = [nltk.word_tokenize(feature) for feature in features]
+
+# 4.3 Lemmatization
+wordnet_lemmatizer = WordNetLemmatizer()
+features = [ [wordnet_lemmatizer.lemmatize(word) for word in feature] for feature in features]
+
+# 4.4 Stop Words Removal
+
+stop_words = set(stopwords.words('english'))
+
+n_documents = len(features)
+for i in range(n_documents):
+    features[i] = [w for w in features[i] if not w in stop_words]
+    s = ""
+    for el in features[i]:
+        s += el + " "
+    features[i] = s[:-1]
+
+# # # #
+# 5. Construct tf_idf matrix
+# # # #
+
+count_vectorizer = CountVectorizer()
+counts_matrix = count_vectorizer.fit_transform(features)
+doc_term_matrix = counts_matrix.todense()
+cosine_similarity_matrix = cosine_similarity(counts_matrix)
+
+# # # #
+# 6. Classify testing data
+# # # #
+
+y_pred = []
+y_true = []
+
+for i in range(9000, n_documents):
+    cosine = {'=Poor=': [0,0], '=Unsatisfactory=': [0,0], '=Good=': [0,0], '=VeryGood=': [0,0], '=Excellent=': [0,0]}
+    for j in range(0, 9000):
+        cosine[labels[j]][0] += cosine_similarity_matrix[i][j]
+        cosine[labels[j]][1] += 1
+    
+    cosine["=Poor="] = cosine["=Poor="][0] / cosine["=Poor="][1]
+    cosine["=Unsatisfactory="] = cosine["=Unsatisfactory="][0] / cosine["=Unsatisfactory="][1]
+    cosine["=Good="] = cosine["=Good="][0] / cosine["=Good="][1]
+    cosine["=VeryGood="] = cosine["=VeryGood="][0] / cosine["=VeryGood="][1]
+    cosine["=Excellent="] = cosine["=Excellent="][0] / cosine["=Excellent="][1]
+    
+    pred = max(cosine, key=cosine.get)
+    y_pred.append(pred)
+    print(pred)
+
+print(accuracy_score(test_labels, y_pred))
+    
---
+++ reviews_naive_bayes.py
@@ -0,0 +1,122 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+# 4th model: naive bayes classifier
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import sys
+from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+# # # #
+# 1. Get train and test sets' files
+# # # #
+
+args = sys.argv[1:]
+
+i = 0
+for arg in args:
+    if arg == '-test':
+        test = args[i + 1]
+    if arg == '-train':
+        train = args[i + 1]
+    i += 1
+    
+# # # #
+# 2. Get train & test features
+# # # #
+
+train_features, test_features, features = [], [], []
+
+with open(train) as f:
+    lines = f.readlines()
+
+for line in lines:
+    feature = line.split()[1:]
+    s = ""
+    for f in feature:
+        s += f + " "
+    s = s[:-1]
+    train_features.append(s)
+    features.append(s)
+
+with open(test) as f:
+    lines = f.readlines()
+
+for line in lines:
+    test_features.append(line)
+    features.append(line)
+
+# # # #
+# 3. Get train & test labels
+# # # #
+
+train_labels = []
+
+with open(train) as f:
+    lines = f.readlines()
+
+for line in lines:
+    label = line.split()[0]
+    train_labels.append(label)
+
+test_labels = []
+
+with open('test_results.txt') as f:
+    lines = f.readlines()
+
+for line in lines:
+    test_labels.append(line[:-1])
+    
+# # # #
+# 4. Process reviews
+# # # #
+
+# 4.1 Lowercasing
+features = [feature.lower() for feature in features]
+
+# 4.2 Tokenization
+features = [nltk.word_tokenize(feature) for feature in features]
+
+# 4.3 Lemmatization
+wordnet_lemmatizer = WordNetLemmatizer()
+features = [ [wordnet_lemmatizer.lemmatize(word) for word in feature] for feature in features]
+
+# 4.4 Stop Words Removal
+
+stop_words = set(stopwords.words('english'))
+
+n_documents = len(features)
+for i in range(n_documents):
+    features[i] = [w for w in features[i] if not w in stop_words]
+    s = ""
+    for el in features[i]:
+        s += el + " "
+    features[i] = s[:-1]
+
+# # # #
+# 5. Prepare features for naive bayes model
+# # # #
+
+count_vectorizer = CountVectorizer()
+
+counts_matrix   = count_vectorizer.fit_transform(features)
+doc_term_matrix = counts_matrix.todense()
+
+print(np.array(doc_term_matrix))
+
+# # # #
+# 6. Learn model
+# # # #
+
+model = MultinomialNB().fit(doc_term_matrix[:9000], train_labels)
+
+# # # #
+# 7. Predict test labels
+# # # #
+y_pred = model.predict(doc_term_matrix[9000:])
+
+print('Accuracy:', accuracy_score(test_labels, y_pred))
